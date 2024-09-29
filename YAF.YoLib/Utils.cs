362196b7890996ee1be06073db9af9736c37112e
snowluwuxx0 <snowluwuxx0@gmail.com>
2024-09-29 16:20:49 +0600

    Добавьте файлы проекта.

---
+++ YAF.YoLib/Utils.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+
+namespace YAF.YoLib
+{
+    public static class Utils
+    {
+        public static void ValidateFormat(BinaryReader reader)
+        {
+            if (new string(reader.ReadChars(3)) != "YAF")
+                throw new Exception("Invalid format! This is not a YAF.");
+        }
+
+        public static Bitmap ReadFrame(BinaryReader reader)
+        {
+            int width = reader.ReadInt32();
+            int height = reader.ReadInt32();
+            Bitmap frame = new Bitmap(width, height);
+            byte[] compressedData = reader.ReadBytes(reader.ReadInt32());
+
+            using (var compressedStream = new MemoryStream(compressedData))
+            using (var gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        int argb = new BinaryReader(gzipStream).ReadInt32();
+                        frame.SetPixel(x, y, Color.FromArgb(argb));
+                    }
+                }
+            }
+
+            return frame;
+        }
+
+        public static void WriteHeader(BinaryWriter writer, int frameCount)
+        {
+            writer.Write(new char[] { 'Y', 'A', 'F' });
+            writer.Write(frameCount);
+        }
+
+        public static void WriteFrame(BinaryWriter writer, Bitmap frame)
+        {
+            writer.Write(frame.Width);
+            writer.Write(frame.Height);
+
+            byte[] compressedData = CompressFrame(frame);
+            writer.Write(compressedData.Length);
+            writer.Write(compressedData);
+        }
+
+        public static byte[] CompressFrame(Bitmap frame)
+        {
+            using (var compressedStream = new MemoryStream())
+            using (var gzipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+            {
+                for (int y = 0; y < frame.Height; y++)
+                {
+                    for (int x = 0; x < frame.Width; x++)
+                    {
+                        int argb = frame.GetPixel(x, y).ToArgb();
+                        byte[] pixelData = BitConverter.GetBytes(argb);
+                        gzipStream.Write(pixelData, 0, pixelData.Length);
+                    }
+                }
+
+                gzipStream.Flush();
+                return compressedStream.ToArray();
+            }
+        }
+
+
+    }
+}
---
+++ YAF.YoLib/YAF.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace YAF.YoLib
+{
+    public static class YAF
+    {
+        public static List<Bitmap> Unpack(byte[] input)
+        {
+            using (var memoryStream = new MemoryStream(input))
+            using (var reader = new BinaryReader(memoryStream))
+            {
+                Utils.ValidateFormat(reader);
+
+                var frames = new List<Bitmap>();
+                int frameCount = reader.ReadInt32();
+
+                for (int i = 0; i < frameCount; i++)
+                {
+                    Bitmap frame = Utils.ReadFrame(reader);
+                    frames.Add(frame);
+                }
+
+                return frames;
+            }
+        }
+
+        public static byte[] Pack(List<Bitmap> frames)
+        {
+            using (var outputStream = new MemoryStream())
+            using (var writer = new BinaryWriter(outputStream))
+            {
+                Utils.WriteHeader(writer, frames.Count);
+
+                foreach (Bitmap frame in frames)
+                {
+                    Utils.WriteFrame(writer, frame);
+                }
+
+                return outputStream.ToArray();
+            }
+        }
+    }
+}
